697e16fc6d103e56df7cd79a40c9b2f2bd56d1c1
ethan1341 <ethan1341@yahoo.com>
2024-05-01 23:00:48 -0400

    added horizontal card

--- frontnine-fe/src/component/common/HCard.tsx
+++ frontnine-fe/src/component/common/HCard.tsx
@@ -1,1 +1,42 @@
-export const HCard:FC.Comp =
+import {Button, Card, CardBody, CardFooter, Stack, Image, Text} from "@chakra-ui/react";
+import {ReactNode} from "react";
+
+type props ={
+    image: string;
+    title: string;
+    description: string;
+}
+
+function Heading(props: { size: string, children: ReactNode }) {
+    return null;
+}
+
+export const HCard:React.FC<props> = ({image,title,description})=>{
+    return(
+        <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='outline'
+            height='auto'
+        >
+            <Image
+                objectFit='cover'
+                maxW={{ base: '100%', sm: '200px' }}
+                h={'130px'}
+                src='./shoot.jpg'
+                alt='Caffe Latte'
+            />
+
+            <Stack>
+                <CardBody>
+                    <Heading size='md'>The perfect latte</Heading>
+
+                    <Text py='2'>
+                        Caffè latte is a coffee beverage of Italian origin made with espresso
+                        and steamed milk.
+                    </Text>
+                </CardBody>
+            </Stack>
+        </Card>
+    )
+}
--- frontnine-fe/src/component/directoryCard/DirectoryCard.tsx
+++ frontnine-fe/src/component/directoryCard/DirectoryCard.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import {Box, Flex, Heading,Text} from "@chakra-ui/react";
+import {Box, Flex, Heading, HStack, Text} from "@chakra-ui/react";
+import { Grid, GridItem } from '@chakra-ui/react'
+import {HCard} from "../common/HCard";
 
 export const DirectoryCard = ()=>{
     return(
         <Box maxW="calc(4/5 * 100%)" marginTop="-100px" zIndex="100" backgroundColor={'gray.100'}  p={5}  boxShadow='dark-lg' rounded='md' textAlign={'center'}>
             <Text fontSize='2xl' mt={5} color={'grey.800'}>Now Entering Boise Golf Hub</Text>
             <Text fontSize='lg' p={5} color={'grey.800'}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text>
+            <HStack>
+            <Box maxW="calc(1/3 * 100%)" p={5}>  <HCard image={'ethan'} title={'ethan'} description={'ethan'}/></Box>
+            <Box maxW="calc(1/3 * 100%)" p={5}>  <HCard image={'ethan'} title={'ethan'} description={'ethan'}/></Box>
+            <Box maxW="calc(1/3 * 100%)" p={5}>  <HCard image={'ethan'} title={'ethan'} description={'ethan'}/></Box>
+            </HStack>
         </Box>
+
     )
 }
